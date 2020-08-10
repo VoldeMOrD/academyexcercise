d1f2f2394c1842c0a5a996af8d42d6a4ae5e77ae
Irwing Antonio Herrera Arends <irwing.herrera@globant.com>
2020-08-10 13:18:52 -0500

    adding menu component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from './modules/angular-material/angular-material.module';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
 @NgModule({
-  declarations: [ HeaderComponent, FooterComponent ],
-  imports: [ CommonModule, AngularMaterialModule ],
-  exports: [ AngularMaterialModule, HeaderComponent ]
+  declarations: [ HeaderComponent, FooterComponent, MenuComponent ],
+  imports: [ CommonModule,AppRoutingModule, AngularMaterialModule ],
+  exports: [ AngularMaterialModule, HeaderComponent, FooterComponent, MenuComponent ]
 })
 export class SharedModule { }
