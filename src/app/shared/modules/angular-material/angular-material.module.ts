7bfdbf6752aa83f76f2c01340ddbafef70f03e69
Irwing Antonio Herrera Arends <irwing.herrera@globant.com>
2020-08-10 23:42:35 -0500

    showing basic list of users

--- src/app/shared/modules/angular-material/angular-material.module.ts
+++ src/app/shared/modules/angular-material/angular-material.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
 @NgModule({
   declarations: [],
-  imports: [ CommonModule, MatToolbarModule, MatButtonModule, MatIconModule ],
-  exports: [ MatToolbarModule, MatButtonModule, MatIconModule ]
+  imports: [ CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule ],
+  exports: [ MatToolbarModule, MatButtonModule, MatIconModule, MatListModule ]
 })
 export class AngularMaterialModule { }
